ec9a5407cb394172b253f5793dbe524ce5d1af94
Nicolas <nico.daddabbo7100@gmail.com>
2024-12-19 15:10:46 +0100

    travail sur la page users et ajout des détails user avec tasks à la demande

--- src/app/admin/components/users/user-list/user-list.component.ts
+++ src/app/admin/components/users/user-list/user-list.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FakeUsersProvider } from '../../../../gateways/adapters/fake-users.provider';
+import { AsyncPipe } from '@angular/common';
+import { UserDetailsComponent } from '../../../../components/users/user-details/user-details.component';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [AsyncPipe, UserDetailsComponent],
   templateUrl: './user-list.component.html',
-  styleUrl: './user-list.component.scss'
+  styleUrl: './user-list.component.scss',
 })
 export class UserListComponent {
+  readonly #usersProvider = inject(FakeUsersProvider);
 
+  users$ = this.#usersProvider.getUsers();
+  showUserDetails = signal({ show: false, id: 0 });
+  showDetails(id: number) {
+    this.showUserDetails.set({ show: true, id });
+  }
+
+  showEditForm() {}
+
+  showCreateForm() {}
 }
