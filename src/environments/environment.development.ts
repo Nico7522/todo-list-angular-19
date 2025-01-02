fdb95d02f8b67c84aba8247deb27a9199c0dbdff
Nicolas <nico.daddabbo7100@gmail.com>
2025-01-02 16:39:25 +0100

    remove env

--- src/environments/environment.development.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  // IMG_URL: 'https://api-task-image.onrender.com',
-  // API_IGM_URL: 'https://api-task-image.onrender.com/upload',
-  IMG_URL: '',
-  API_IGM_URL: '',
-};
