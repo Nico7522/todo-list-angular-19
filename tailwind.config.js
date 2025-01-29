4db22042319122716fbc5a9c532e250adf111769
Nicolas <nico.daddabbo7100@gmail.com>
2025-01-29 21:53:10 +0100

    dd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  darkMode: 'media',
-  content: ["./src/**/*.{html, ts, js}","./node_modules/flowbite/**/*.js"],
+  darkMode: "media",
+  content: ["./src/**/*.{html, ts, js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
-      
       fontFamily: {
-        'lato': ['lato', 'sans-serif'],
+        lato: ["lato", "sans-serif"],
       },
       colors: {
-        'default': 'rgb(24, 24, 24)',
-        'onHover': 'rgb(29, 29, 29)'
+        default: "rgb(24, 24, 24)",
+        onHover: "rgb(29, 29, 29)",
       },
       screens: {
-        'nav': '1400px'
-      }
+        nav: "1400px",
+      },
     },
-
   },
   plugins: [
-    require('flowbite/plugin') // add this line
+    require("flowbite/plugin"), // add this line
   ],
-}
-
+};
